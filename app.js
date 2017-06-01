3f26334a79cfaa9608f7766ffaa2b9a3023e07b8
viktorpts <viktor.pts@abv.bg>
2017-06-01 17:54:19 +0300

    Implemented duck function

--- app.js
+++ app.js
@@ -75,6 +75,11 @@
                 person.velx = constants.maxSpeed;
             }
         }
+        if (keysDown["ArrowDown"]) {
+            person.duck = true;
+        } else {
+            person.duck = false;
+        }
         if (keysDown["Space"]) {
             if (person.onGround) {
                 person.onGround = false;
@@ -101,7 +106,11 @@
 
         ctx.fillStyle = "green";
         ctx.translate(person.x, person.y);
-        ctx.fillRect(-25, -100, 50, 100);
+        if (person.duck) {
+            ctx.fillRect(-25, -50, 50, 50);
+        } else {
+            ctx.fillRect(-25, -100, 50, 100);
+        }
 
         ctx.restore();
     }
